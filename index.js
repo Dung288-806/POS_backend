caa6ee1826ce0d47e3999fb68bb1c8e3ae77cf3d
Dao-Dung <daoadung69@gmail.com>
2020-06-12 09:14:04 +0700

    add PORT

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const UserRoute = require('./routes/user.route')
 const cors = require("cors");
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
